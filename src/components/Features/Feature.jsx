3c1929beca6d4be8ecea09aab87e9a2f5ce78f77
Tomasz <tomasz.iwanicki@onet.pl>
2023-01-09 16:29:49 +0100

    Create structure for Feature component

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './Feature.css'
 
-const Feature = () => {
-  return <div>Feature</div>
+const Feature = (props) => {
+  return (
+    <div className="feature">
+      <div className="feature-image">
+        <img src={props.image} alt="" />
+      </div>
+      <div className="feature-content">
+        <h2 className="feature-title">{props.title}</h2>
+        <p className="feature-description">{props.description}</p>
+        <div className="feature-buttons">
+          <button className="feature-button">More Info</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Feature
