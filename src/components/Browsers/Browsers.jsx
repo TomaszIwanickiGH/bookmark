91b51ef659bffffc3be45b463b60d945d7b1faee
Tomasz <tomasz.iwanicki@onet.pl>
2023-01-09 17:52:36 +0100

    Create structure for Browsers component

--- src/components/Browsers/Browsers.jsx
+++ src/components/Browsers/Browsers.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import './Browsers.css'
 
+import { data, images } from '../../constants'
+
+import Browser from './Browser'
+
 const Browsers = () => {
-  return <div>Browsers</div>
+  return (
+    <div className="browsers-container">
+      <div className="browsers-header">
+        <h2 className="browsers-title">Download the extension</h2>
+        <p className="browsers-description">
+          We've got more browsers in the pipeline. Please do let us know if
+          you've got a favourite you'd like us to prioritize.
+        </p>
+      </div>
+      <div className="browsers-browsers-types">
+        {data.browsers.map((browser) => (
+          <div
+            className="browsers-browser"
+            style={{ marginTop: browser.marg }}
+            key={browser.id}
+          >
+            <Browser
+              image={browser.image}
+              title={browser.title}
+              desc={browser.description}
+            />
+            <img src={images.bgDots} alt="dots" />
+            <button className="browser-button">Add & Install Extension</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Browsers
