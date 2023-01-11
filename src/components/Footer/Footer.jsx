91cb48ad3263a420376e06f333a8a0c28996ec5f
Tomasz <tomasz.iwanicki@onet.pl>
2023-01-11 18:30:10 +0100

    Create footer structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Footer.css'
 
+import { images } from '../../constants'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="footer">
+      <div className="footer-nav">
+        <img src={images.logoBookmarkMobile} alt="logo" />
+        <div className="footer-links">
+          <ul>
+            <li className="footer-link">features</li>
+            <li className="footer-link">pricing</li>
+            <li className="footer-link">contact</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-socials">
+        <img src={images.iconFacebook} alt="facebook" />
+        <img src={images.iconTwitter} alt="twitter" />
+      </div>
+    </div>
+  )
 }
 
 export default Footer
