152b09851b39fde593f00528fe102b2e017e89f2
Tomasz <tomasz.iwanicki@onet.pl>
2023-01-11 17:20:09 +0100

    Create newsletter structure

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 import './Newsletter.css'
 
 const Newsletter = () => {
-  return <div>Newsletter</div>
+  return (
+    <div className="newsletter">
+      <div className="newsletter-info">
+        <p>
+          <span>35,000+</span> already joined
+        </p>
+      </div>
+      <div className="newsletter-header">
+        <h2>
+          Stay up-to-date with what
+          <br /> we're doing
+        </h2>
+      </div>
+      <div className="newsletter-form">
+        <input type="text" placeholder="Enter your email adress" />
+        <button>Contact Us</button>
+      </div>
+    </div>
+  )
 }
 
 export default Newsletter
